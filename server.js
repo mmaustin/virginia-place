463f1af5752ef33f8d66b28ecdd6411e60b3a0e5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-14 10:24:19 -0500

    imported and applied not found middleware in serverjs

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const app = express();
 
+import notFoundMiddleware from './middleware/not-found.js';
+
 app.get('/', (req, res) => {
     res.send('Welcome');
 })
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5001
 
