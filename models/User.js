8b24735aee24a6bba97ea52adc2c2d68f9937478
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-15 09:20:36 -0500

    installed validator and imported, and implemented in the user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -11,10 +12,10 @@ const UserSchema = mongoose.Schema({
       email: {
         type: String,
         required: [true, 'Please provide email'],
-        match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please provide a valid email',
-        ],
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide a valid email',
+        },
         unique: true,
       },
       password: {
