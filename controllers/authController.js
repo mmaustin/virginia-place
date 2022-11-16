053b2f4162af1037235bcc282e11d5666d54045a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-16 09:36:08 -0500

    completed build out of register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,29 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import {BadRequestError, UnAuthenticatedError} from '../errors/index.js'
 
 const register = async (req, res) => {
-    const {name, email, password} = req.body;
-    const user = await User.create({name, email, password});
-    res.status(StatusCodes.CREATED).json({user});
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+      throw new BadRequestError('please provide all values')
+    }
+    const userAlreadyExists = await User.findOne({ email })
+    if (userAlreadyExists) {
+      throw new BadRequestError('Email already in use')
+    }
+    const user = await User.create({ name, email, password })
+  
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({
+      user: {
+        name: user.name,
+        email: user.email,
+        buildingNumber: user.buildingNumber
+      },
+      token,
+      buildingNumber: user.buildingNumber
+    })
 }
 
 const login = async (req, res) => {
