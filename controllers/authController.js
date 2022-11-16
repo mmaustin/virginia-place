a7f4fdd7d8534da62df7b23704529b6cf4cc1f8b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-16 09:24:56 -0500

    installed bcryptjs and jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res) => {
     const {name, email, password} = req.body;
     const user = await User.create({name, email, password});
-    res.status(201).json({user});
+    res.status(StatusCodes.CREATED).json({user});
 }
 
 const login = async (req, res) => {
