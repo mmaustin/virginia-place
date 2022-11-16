2bb3d5b15610fd8870765f39e0a93e3ed38f886c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-16 09:42:56 -0500

    added three user schema functions on the user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -31,5 +33,23 @@ const UserSchema = mongoose.Schema({
         default: 'Y\'all already know',
       }    
 })
+
+UserSchema.pre('save', async function () {
+  // console.log(this.modifiedPaths())
+  if (!this.isModified('password')) return
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
 
 export default mongoose.model('User', UserSchema);
