f77df1db4ad56637e6b8a30637bd175351263bde
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-19 12:05:37 -0500

    completed basic landing page set up

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,27 @@
 import main from '../assets/images/main.svg';
-
+//import {Link} from 'react-router-dom';
 const Landing = () => {
   return (
-    <>
-        <div>Landing Page</div>
-        <img src={main} alt='event scene' className='img'/>
-    </>
+    <main>
+        <nav>
+            <p> Logo Landing Page</p>
+        </nav>
+        <div className='container'>
+            <div className='info'>
+                <h2>
+                    Events Page
+                </h2>
+                <p>
+                    Swag typewriter pour-over whatever farm-to-table edison bulb.
+                    Unicorn edison bulb whatever gatekeep, pok pok health goth cronut
+                    chambray keffiyeh. Health goth vinyl DIY pork belly skateboard
+                    snackwave, artisan same everyday carry church-key farm-to-table kombucha.
+                </p>
+                <button className='btn btn-hero'> Login/Register</button>
+            </div>
+            <img src={main} alt='event scene' className='img'/>
+        </div>
+    </main>
   )
 }
 export default Landing
