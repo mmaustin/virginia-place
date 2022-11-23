492f0039f8ee12e97a5cbb0266958eb2cfbeae7b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-23 09:42:29 -0500

    added local state values to register component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,17 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+const localState = {
+  name: '',
+  email: '',
+  password: '',
+  buildingNumber: ''
+}
+
+
 const Register = () => {
+  const [values, setValue] = useState(localState);
+
   return (
     <div>Register</div>
   )
