c4977b534f4adb79a892f0848379322ac8376199
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-29 09:37:45 -0500

    completed form row component for register page

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,8 +1,13 @@
 
 
-const FormRow = () => {
+const FormRow = ({labelText, type, name, value, handleChange}) => {
   return (
-    <div>FormRow</div>
+    <div>
+        <label htmlFor={name} className='form-label' >
+            {labelText || name}
+        </label>
+        <input type={type} id={name} name={name} value={value} onChange={handleChange} className='form-input' />
+    </div>
   )
 }
 export default FormRow
