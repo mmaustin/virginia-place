4260815e6921cb4828dcc76b554f416a6875e4b0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-30 11:47:13 -0500

    set up reducer logic for display and clear alerts

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -19,10 +19,15 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const displayAlert = () => {
+        dispatch({type: DISPLAY_ALERT});
+    }
+
     return(
         <AppContext.Provider 
             value={{
-                ...state
+                ...state,
+                displayAlert,
             }}
         >
             {children}
