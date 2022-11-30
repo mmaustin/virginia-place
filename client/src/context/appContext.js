1d6e32ab3f046e23b46a5f26c10c0ef0e4263b01
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-30 10:05:04 -0500

    set up initial state in app context file

---
+++ client/src/context/appContext.js
@@ -0,0 +1,19 @@
+import React, { useContext, useReducer } from "react";
+
+
+const initialState = {
+    isLoading: false,
+    showAlert: false,
+    alertText: '',
+    alertType: '',    
+}
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+
+
+    return(
+        <></>
+    )
+}
