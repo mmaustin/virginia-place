753d0e5361e06b8dc3c010c0e6fd83c5f9fbd6d7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-06 11:58:19 -0500

    created a temporary logo component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import {FiHome} from 'react-icons/fi'
 
 const Navbar = () => {
   return (
     <Wrapper>
         <h3>Navbar</h3>
+        <FiHome/>
     </Wrapper>
   )
 }
