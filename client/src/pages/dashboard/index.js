61291a960057ebd58183c9c3e070e61521b9081d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-06 09:23:44 -0500

    imported dashboard files to index file

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,4 +1,11 @@
-
+import AddJob from './AddJob';
+import AllJobs from './AllJobs';
+import Profile from './Profile';
+import SharedLayout from './SharedLayout';
 
 export{
+    AddJob,
+    AllJobs,
+    Profile,
+    SharedLayout
 };
