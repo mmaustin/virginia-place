f7aedfad2de1e6ff41a374ff67798d6bc55191b1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-06 09:54:17 -0500

    set up nested component/route structure in app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Landing, Error, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
 import { AllJobs, AddJob, Profile, SharedLayout } from './pages/dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>          
         <Route path='/' element={<div>Dashboard</div>} />
         <Route path='/landing' element={<Landing/>} />
         <Route path='/register' element={<Register/>} />
