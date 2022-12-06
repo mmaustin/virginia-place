f1b56a5f6fb326f06375edf8943040585a285da1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-06 09:31:21 -0500

    imported dashboard pages to app js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Landing, Error, Register } from './pages';
+import { AllJobs, AddJob, Profile, SharedLayout } from './pages/dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>} />
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/landing' element={<Landing/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='*' element={<Error/>} />
       </Routes>
