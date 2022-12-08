3036a2a9a8566c94a263b4d43d969085c90714bf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-08 11:01:18 -0500

    imported components and set up use app context and use state in profile component

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+import {Wrapper} from '../../assets/wrappers/DashboardFormPage';
+import {useAppContext} from '../../context/appContext';
+import {FormRow, Alert} from '../../components';
+
 const Profile = () => {
+  const {user, showAlert, displayAlert, updateUser, isLoading} = useAppContext();
+
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [buildingNumber, setBuildingNumber] = useState(user?.buildingNumber);
+
+  
+
   return (
     <h3>Profile</h3>
   )
