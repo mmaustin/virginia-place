0fc35af1c25b446f33978203e78b6aa45e57cdb4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-08 11:37:39 -0500

    completed profile component form

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -20,7 +20,36 @@ const Profile = () => {
   }
 
   return (
-    <h3>Profile</h3>
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        {showAlert && <Alert />}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            type='email'
+            name='email'
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            type='text'
+            labelText='Building Number'
+            name='buildingNumber'
+            value={buildingNumber}
+            handleChange={(e) => setBuildingNumber(e.target.value)}
+          />
+          <button className='btn btn-block' type='submit' disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 export default Profile
