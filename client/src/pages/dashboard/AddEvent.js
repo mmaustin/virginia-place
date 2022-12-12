e6a5bf55c67a0857d6c324e5f6660c3bdb53c3b9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-12 11:33:58 -0500

    added a temporary handle event input function in add form component

--- client/src/pages/dashboard/AddEvent.js
+++ client/src/pages/dashboard/AddEvent.js
@@ -4,9 +4,30 @@ import { useAppContext } from '../../context/appContext';
 
 
 const AddEvent = () => {
-  const {showAlert, displayAlert, organizer, eventType, description, } = useAppContext();
+  const {showAlert, displayAlert, organizer, eventType, description, isEditing } = useAppContext();
+
+  const handleEventInput = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name + ' ' + value);
+  }
+
   return (
-    <h3>AddEvent</h3>
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'Edit Event' : 'Add Event'}</h3>
+        {showAlert && <Alert/>}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='organizer'
+            value={organizer}
+            handleChange={handleEventInput}
+          />
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 export default AddEvent
