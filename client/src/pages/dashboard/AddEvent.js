ba5a36f62b452cc178c0c4f23a745c69e2e2c634
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-12 11:02:01 -0500

    imported use app context and a few other components to add job page

--- client/src/pages/dashboard/AddEvent.js
+++ client/src/pages/dashboard/AddEvent.js
@@ -1,4 +1,10 @@
+import {FormRow, Alert} from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useAppContext } from '../../context/appContext';
+
+
 const AddEvent = () => {
+  const {showAlert, displayAlert, organizer, eventType, description, } = useAppContext();
   return (
     <h3>AddEvent</h3>
   )
