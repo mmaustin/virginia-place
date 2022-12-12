6f9de6abe041fb5496c778b39db85518b48beb74
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-12 12:13:03 -0500

    added buttons with temporary logging functionality

--- client/src/pages/dashboard/AddEvent.js
+++ client/src/pages/dashboard/AddEvent.js
@@ -6,6 +6,11 @@ import { useAppContext } from '../../context/appContext';
 const AddEvent = () => {
   const {showAlert, displayAlert, organizer, eventType, description, isEditing } = useAppContext();
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('event created')
+  }
+
   const handleEventInput = (e) => {
     e.preventDefault();
     const name = e.target.name;
@@ -25,6 +30,43 @@ const AddEvent = () => {
             value={organizer}
             handleChange={handleEventInput}
           />
+          <FormRow
+            type='text'
+            name='eventType'
+            value={eventType}
+            handleChange={handleEventInput}
+          />
+          <label htmlFor="description" className='form-label'>Description</label>
+          <textarea
+            className='form-description'
+            id='description'
+            type='text'
+            name='description'
+            value={description}
+            onChange={handleEventInput}
+          />
+          <div className='btn-container'>
+            <button
+              type='submit'
+              className='btn btn-block submit-btn'
+              onClick={handleSubmit}
+              // disabled={isLoading}
+            >
+              submit
+            </button>
+            <button
+              className='btn btn-block clear-btn'
+              onClick={(e)=> {
+                e.preventDefault()
+                console.log('right on!')}}              
+              // onClick={(e) => {
+              //   e.preventDefault()
+              //   clearValues()
+              // }}
+            >
+              clear
+            </button>
+          </div>          
         </div>
       </form>
     </Wrapper>
