2408d3f7d699b6f5e82e0011416cb833c268080a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-13 10:30:24 -0500

    imported required assets into events container component

--- client/src/components/EventsContainer.js
+++ client/src/components/EventsContainer.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { useAppContext } from "../context/appContext";
+import Wrapper from '../assets/wrappers/EventsContainer';
+import Loading from "./Loading";
+import Event from './Event';
+
 const EventsContainer = () => {
+  const {} = useAppContext();
   return (
     <div>EventsContainer</div>
   )
