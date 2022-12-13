2318dde18262e1a48e949dd2bcc7f48f39e49f12
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-13 10:47:27 -0500

    activated the loading component in events container

--- client/src/components/EventsContainer.js
+++ client/src/components/EventsContainer.js
@@ -5,9 +5,29 @@ import Loading from "./Loading";
 import Event from './Event';
 
 const EventsContainer = () => {
-  const {} = useAppContext();
+  const {events, getEvents, isLoading, totalEvents, pages} = useAppContext();
+  
+  useEffect(() => {
+    getEvents()
+    // eslint-disable-next-line
+  }, [])
+
+  if(isLoading){
+    return <Loading center />
+  }
+
+  if(events.length === 0){
+    return(
+      <Wrapper>
+        <h3>No Events To Display . . . </h3>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>EventsContainer</div>
+    <div>
+      
+    </div>
   )
 }
 export default EventsContainer
