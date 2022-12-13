95c6341b0dd1efc405fef7a52c686805829b2fca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-13 11:21:25 -0500

    imported moment js and configured the event created at date

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,6 +1,15 @@
-const Event = ({organizer, eventType, description}) => {
+import moment from 'moment'
+
+const Event = ({organizer, eventType, description, createdAt}) => {
+  let date = moment(createdAt);
+  date = date.format('MMM Do, YYYY')
+
   return (
-    <h5>{eventType}</h5>
+    <div>
+      <h5>{eventType}</h5>
+      <h5>{date}</h5>
+      <p>{description}</p>
+    </div>
   )
 }
 export default Event
