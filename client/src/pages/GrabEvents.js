50cf7ee48c5a1b37464f4d7611c0aeedc5f2d606
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-19 11:20:34 -0500

    completed the non-return portion of grab events component

--- client/src/pages/GrabEvents.js
+++ client/src/pages/GrabEvents.js
@@ -1,13 +1,29 @@
 import { useEffect } from "react";
 import { useAppContext } from "../context/appContext";
-
+import Wrapper from "../assets/wrappers/EventsContainer";
+import Loading from "../components/Loading";
+import Event from "../components/Event";
 
 const GrabEvents = () => {
     const {allEvents, allTotalEvents, numberOfPages, grabEvents, isLoading} = useAppContext();
 
     useEffect(() => {
       grabEvents();
+      // eslint-disable-next-line
     }, [])
+
+    if(isLoading){
+        return <Loading center />
+      }
+    
+      if(allEvents.length === 0){
+        return(
+          <Wrapper>
+            <h3>No Events To Display . . . </h3>
+          </Wrapper>
+        )
+      }    
+
   return (
     <>
         <h3>Grab Events</h3>
