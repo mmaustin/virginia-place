0b5877ee6330cf722fcae86169bd53e83ef499b2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-12-20 11:57:25 -0500

    added a link to all events in the sidebars

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -1,9 +1,10 @@
-import { MdQueryStats } from 'react-icons/md'
-import { FaWpforms } from 'react-icons/fa'
+import { SiBookmeter } from 'react-icons/si'
+import { FaWpforms, FaEdgeLegacy } from 'react-icons/fa'
 import { ImProfile } from 'react-icons/im'
 
 const links = [
-  { id: 2, text: 'all events', path: '/', icon: <MdQueryStats /> },
+  { id: 1, text: 'all events', path: '/events', icon: <SiBookmeter /> },
+  { id: 2, text: 'your events', path: '/', icon: <FaEdgeLegacy /> },
   { id: 3, text: 'add event', path: 'add-event', icon: <FaWpforms /> },
   { id: 4, text: 'profile', path: 'profile', icon: <ImProfile /> },
 ]
