54a6c872c87e70e4f9f8c91a6ed98b2a05872e0f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-03 09:51:29 -0500

    built client folder and provided server access to the build folder

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 import morgan from 'morgan'
 
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+
 import connectDB from './db/connect.js';
 
 import authRouter from './routes/authRoutes.js';
@@ -12,16 +17,23 @@ import eventRouter from './routes/eventsRoutes.js';
 
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
-//import authenticateUser from './middleware/auth.js';
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/events', eventRouter);
+
+app.get('*', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.htmml'))
+})
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
